23d07bd3691a088af7b78af56daacb75929236ad
humphreydev5 <humphreyikhalea6@gmail.com>
2023-09-05 14:25:40 +0000

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(letter), end='')
+
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
+
