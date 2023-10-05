c8cb2a7b1b2413993358fa0eb4e802fbd60c33c3
humphreydev5 <humphreyikhalea6@gmail.com>
2023-10-05 10:04:36 +0000

    6-max_integer.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # 5-text_indentation.py
 """Defines a text-indentation function."""
 
@@ -13,3 +14,16 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
