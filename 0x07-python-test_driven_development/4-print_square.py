ca4fb5b44bfba1cd4accc82cccfdec46e8296aa2
humphreydev5 <humphreyikhalea6@gmail.com>
2023-10-05 10:07:03 +0000

    6-max_integer.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,3 +14,8 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
