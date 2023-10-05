ae94ab76165229571f0c88a19b7c6dfe4362af5c
humphreydev5 <humphreyikhalea6@gmail.com>
2023-10-05 09:19:29 +0000

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+# 5-text_indentation.py
+"""Defines a text-indentation function."""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
