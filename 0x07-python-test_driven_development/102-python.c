44db14699e6c44beeb7fc17e801e44285efcb3ce
humphreydev5 <humphreyikhalea6@gmail.com>
2023-10-05 09:38:01 +0000

    100-matrix_mul.py

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
+
+/**
+ * print_python_string - Prints string information
+ *
+ * @p: Python Object
+ * Return: no return
+ */
+void print_python_string(PyObject *p)
+{
+
+	PyObject *str, *repr;
+
+	(void)repr;
