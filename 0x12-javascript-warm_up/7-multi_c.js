3695027c5be3fa8fe0d2b76dc66dc9c1a7244acc
humphreydev5 <humphreyikhalea6@gmail.com>
2023-12-13 16:38:49 -0800

    javascript-warm_up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
@@ -9,4 +10,3 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
     i++;
   }
 }
-
