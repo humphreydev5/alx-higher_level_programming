78aee66b7c7fc32ccc93fb36a92eb84b5e217c2d
humphreydev5 <humphreyikhalea6@gmail.com>
2023-12-13 16:14:27 -0800

    Object Scopes and Closures Commit

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 const dict = require('./101-data').dict;
 
 const totalist = Object.entries(dict);
@@ -15,4 +16,3 @@ for (const j in valsUniq) {
   newDict[valsUniq[j]] = list;
 }
 console.log(newDict);
-
